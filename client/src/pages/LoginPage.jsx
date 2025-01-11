a9cb1a92aa358ae2d896e9d95e9c654bb862bf4b
devanshi <165299114+devanshi2121@users.noreply.github.com>
2025-01-11 19:50:37 +0530

    Login-Page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,12 @@
 export default function LoginPage() {
     return (
-        <div>
-            login
+        <div className="mt-4">
+            <h1 className="text-4xl text-center">Login</h1>
+            <form className="max-w-md mx-auto border">
+                <input type="email" placeholder="your@gmail.com"/>
+                <input type="password" placeholder="password"/>
+                <button>Login</button>
+            </form>
         </div>
     );
 }
