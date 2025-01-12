1d91c9b0f68fb94f39a83bb51d035608666da7ab
Chetna1405 <122478476+Chetna1405@users.noreply.github.com>
2025-01-12 15:10:53 +0530

    updated w1

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -5,12 +5,13 @@ export default function LoginPage() {
         <div className="mt-4 grow items-center justify-around">
             <div className="mb-64">
                 <h1 className="text-4xl text-center mb-4">Login</h1>
-                <form className="max-w-md mx-auto border">
+                <form className="max-w-md mx-auto">
                     <input type="email" placeholder="your@gmail.com" />
                     <input type="password" placeholder="password" />
                     <button className="primary">Login</button>
                     <div className="text-center py-2 text-gray-500">
-                        have an account yet? <Link className="underline text-black" to={'/register'}>Register now</Link>
+                        Don&apos;t have an account yet?
+                        <Link className="underline text-black" to={'/register'}>Register now</Link>
                     </div>
                 </form>
             </div>
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -5,7 +5,7 @@ export default function RegisterPage() {
         <div className="mt-4 grow items-center justify-around">
             <div className="mb-64">
                 <h1 className="text-4xl text-center mb-4">Register</h1>
-                <form className="max-w-md mx-auto border">
+                <form className="max-w-md mx-auto">
                     <input type="text" placeholder="John Doe" />
                     <input type="email" placeholder="your@gmail.com" />
                     <input type="password" placeholder="password" />
