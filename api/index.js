84eb043479edc413c41d0e1a3d18d30bd6e122d1
Chetna1405 <122478476+Chetna1405@users.noreply.github.com>
2025-01-18 16:53:04 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.json());
 
 app.use(cors({
-    credentials: true ,
+    credentials: true,
     origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type'],
@@ -14,15 +14,13 @@ app.use(cors({
 
 
 app.get('/test', (req, res) => {
-    console.log("hello")
     res.json('text ok')
 })
 
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
-    console.log(name);
-    res.json({name , email , password});
-} )
+    res.json({ name, email, password });
+})
 
 app.listen(4000, () => {
     console.log("app is running");
