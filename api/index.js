45cf30de0c7ba32645ad2d425aa30d73dd3bcbc7
Srishti Gupta <132117020+srishtii017@users.noreply.github.com>
2025-01-20 23:51:03 +0530

    jwt

--- api/index.js
+++ api/index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const cors = require('cors')
 const mongoose=require("mongoose")
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('./models/user.js');
 
 require('dotenv').config()
 const app = express()
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret ='dbhcxhjasijij';
 
 app.use(express.json());
 
@@ -29,13 +31,38 @@ app.get('/test', (req, res) => {
 app.post('/register', async (req, res) => {
     const { name, email, password } = req.body;
     // res.json({ name, email, password });
+    try{
     const userDoc=await User.create({
         name,
         email,
         password:bcrypt.hashSync(password,bcryptSalt),
     });
     res.json(userDoc);
+} catch(e){
+  res.status(422).json(e);
+}
 })
+app.post('/login', async (req,res)=>{
+    const{email,password} = req.body;
+     const userDoc= await User.findOne({email});
+     if(userDoc){
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if(passOk){
+           jwt.sign({email:userDoc.email , id:userDoc._id},jwtSecret, {}, (err,token)=>{
+            if(err) throw err;
+            res.cookie('token',token).json('pass ok');
+
+           });
+           
+        }
+        else{
+            res.status(422).json('pass not ok');
+        }
+     }
+     else{
+        res.json('not found');
+     }
+});
 
 app.listen(4000, () => {
     console.log("app is running");
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -7,13 +7,19 @@ export default function RegisterPage() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    function registerUser(ev) {
+     async function registerUser(ev) {
         ev.preventDefault();
-        axios.post('/register', {
+        try{
+        await axios.post('/register', {
             name,
             email,
             password,
-        })
+        });
+        alert("Registration successful, Now you can log in");
+    }
+    catch(e){
+        alert('Registration failed , please try again later');
+    }
     }
     
     return (
