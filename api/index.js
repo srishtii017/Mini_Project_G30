ad23e07794b736773fba4d236bfb74a2d1d59136
devanshi <165299114+devanshi2121@users.noreply.github.com>
2025-01-20 19:52:16 +0530

    Database Connect

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose=require("mongoose")
+const bcrypt = require('bcryptjs');
+const User = require('./models/user.js');
+
+require('dotenv').config()
 const app = express()
+
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 app.use(express.json());
 
@@ -11,15 +18,23 @@ app.use(cors({
     allowedHeaders: ['Content-Type'],
 }));
 
-
+console.log(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL);
+//sUiUNw5tERaataPE
 
 app.get('/test', (req, res) => {
     res.json('text ok')
 })
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
     const { name, email, password } = req.body;
-    res.json({ name, email, password });
+    // res.json({ name, email, password });
+    const userDoc=await User.create({
+        name,
+        email,
+        password:bcrypt.hashSync(password,bcryptSalt),
+    });
+    res.json(userDoc);
 })
 
 app.listen(4000, () => {
