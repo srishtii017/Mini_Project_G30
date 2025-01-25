8a144509308a2e4243ef9026b22e0ea5dca98e9e
Chetna1405 <122478476+Chetna1405@users.noreply.github.com>
2025-01-25 17:04:16 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,12 @@
-
-import { Route, Routes ,UserContextProvider } from 'react-router-dom'
 import './App.css'
-import IndexPage from './pages/IndexPage'
-import LoginPage from './pages/LoginPage'
-import Layout from './Layout'
-import RegisterPage from './pages/RegisterPage'
-import axios from 'axios';
-
-axios.defaults.baseURL = 'http://127.0.0.1:4000';
-axios.defaults.withCredentials=true;
 
 function App() {
+
+
   return (
-    <UserContextProvider>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<IndexPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Route>
-    </Routes>
-    </UserContextProvider>
+    <div>
+        
+    </div>
   )
 }
 
