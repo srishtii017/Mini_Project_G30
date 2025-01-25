e15fa2e2d4d7a95ae3cfbf6a8f39600031086465
Chetna1405 <122478476+Chetna1405@users.noreply.github.com>
2025-01-25 12:57:21 +0530

    Revert "Update index.js"
    This reverts commit d9b420c92542beb995f4c994b32f6ed9de872dd8.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const mongoose = require("mongoose")
+const mongoose=require("mongoose")
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
@@ -9,7 +9,7 @@ require('dotenv').config()
 const app = express()
 
 const bcryptSalt = bcrypt.genSaltSync(10);
-const jwtSecret = 'dbhcxhjasijij';
+const jwtSecret ='dbhcxhjasijij';
 
 app.use(express.json());
 
@@ -31,39 +31,38 @@ app.get('/test', (req, res) => {
 app.post('/register', async (req, res) => {
     const { name, email, password } = req.body;
     // res.json({ name, email, password });
-    try {
-        const userDoc = await User.create({
-            name,
-            email,
-            password: bcrypt.hashSync(password, bcryptSalt),
-        })
-        console.log("hello world");
-        console.log(userDoc)
-        res.json(userDoc);
-    } catch (e) {
-        res.status(422).json(e);
-    }
+    try{
+    const userDoc=await User.create({
+        name,
+        email,
+        password:bcrypt.hashSync(password,bcryptSalt),
+    })
+    console.log(userDoc)
+    res.json(userDoc);
+} catch(e){
+  res.status(422).json(e);
+}
 })
-app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    const userDoc = await User.findOne({ email });
-    if (userDoc) {
+app.post('/login', async (req,res)=>{
+    const{email,password} = req.body;
+     const userDoc= await User.findOne({email});
+     if(userDoc){
         const passOk = bcrypt.compareSync(password, userDoc.password);
-        if (passOk) {
-            jwt.sign({ email: userDoc.email, id: userDoc._id }, jwtSecret, {}, (err, token) => {
-                if (err) throw err;
-                res.cookie('token', token).json(userDoc);
+        if(passOk){
+           jwt.sign({email:userDoc.email , id:userDoc._id},jwtSecret, {}, (err,token)=>{
+            if(err) throw err;
+            res.cookie('token',token).json(userDoc);
 
-            });
-
+           });
+           
         }
-        else {
+        else{
             res.status(422).json('pass not ok');
         }
-    }
-    else {
+     }
+     else{
         res.json('not found');
-    }
+     }
 });
 
 app.listen(4000, () => {
