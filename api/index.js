eab2f8a02ef8d0673b729cf05eeb04b433f3bded
Chetna1405 <122478476+Chetna1405@users.noreply.github.com>
2025-01-25 12:59:03 +0530

    Revert "Database Connect"
    This reverts commit ad23e07794b736773fba4d236bfb74a2d1d59136.

--- api/index.js
+++ api/index.js
@@ -2,14 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const mongoose=require("mongoose")
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('./models/User.js');
+const User = require('./models/user.js');
 
 require('dotenv').config()
 const app = express()
 
 const bcryptSalt = bcrypt.genSaltSync(10);
-const jwtSecret ='dbhcxhjasijij';
 
 app.use(express.json());
 
@@ -20,7 +18,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type'],
 }));
 
-// console.log(process.env.MONGO_URL);
+console.log(process.env.MONGO_URL);
 mongoose.connect(process.env.MONGO_URL);
 //sUiUNw5tERaataPE
 
@@ -28,42 +26,16 @@ app.get('/test', (req, res) => {
     res.json('text ok')
 })
 
-app.post('/register', async (req, res) => {
+app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
     // res.json({ name, email, password });
-    try{
     const userDoc=await User.create({
         name,
         email,
         password:bcrypt.hashSync(password,bcryptSalt),
-    })
-    console.log(userDoc)
+    });
     res.json(userDoc);
-} catch(e){
-  res.status(422).json(e);
-}
 })
-app.post('/login', async (req,res)=>{
-    const{email,password} = req.body;
-     const userDoc= await User.findOne({email});
-     if(userDoc){
-        const passOk = bcrypt.compareSync(password, userDoc.password);
-        if(passOk){
-           jwt.sign({email:userDoc.email , id:userDoc._id},jwtSecret, {}, (err,token)=>{
-            if(err) throw err;
-            res.cookie('token',token).json(userDoc);
-
-           });
-           
-        }
-        else{
-            res.status(422).json('pass not ok');
-        }
-     }
-     else{
-        res.json('not found');
-     }
-});
 
 app.listen(4000, () => {
     console.log("app is running on port 4000");
