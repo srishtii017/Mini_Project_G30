4b1c510a5952e576a1e4088634455936f5b3fd71
Srishti Gupta <132117020+srishtii017@users.noreply.github.com>
2025-01-30 00:26:53 +0530

    perks

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,8 +1,39 @@
 import {Link, useParams} from "react-router-dom";
+import {useState} from "react";
+import Perks from "../Perks";
 
 export default function PlacesPage() {
     const {action} = useParams();
-    
+    const [title,setTitle] = useState('');
+    const [address,setAddress] = useState('');
+    const [addedPhotos,setAddedPhotos] = useState([]);
+    const [photoLink,setPhotoLink] = useState('');
+    const [description,setDescription] = useState('');
+    const [perks,setPerks] = useState([]);
+    const [extraInfo,setExtraInfo] = useState('');
+    const [checkIn,setCheckIn] = useState('');
+    const [checkOut,setCheckOut] = useState('');
+    const [maxGuests,setMaxGuests] = useState(1);
+    function inputHeader(text){
+        return(
+            <h2 className="text-2xl mt-4">{text}</h2>
+        );
+    }
+    function inputDescription(text){
+        return(
+            <p className="text-gray-500 text-sm">{text}</p>
+
+        );
+    }
+    function preInput(header,description){
+        return(
+            <>
+            {inputHeader(header)}
+            {inputDescription(description)}
+            </>
+
+        );
+    }
     
     return (
         <div>
@@ -19,22 +50,60 @@ export default function PlacesPage() {
             {action ==='new' && (
                 <div>
                     <form>
-                        <h2 className="text-2xl mt-4">Title</h2>
-                        <p className="text-gray-500 text-sm">Title for your Place, should be short and catchy as in advertisement</p>
+                        {preInput('Title','Title for your Place, should be short and catchy as in advertisement')}
+                       
                         <input type="text" placeholder="title, for example: My Lovely Apt"/>
-                        <h2 className="text-2xl mt-4">Address</h2>
-                        <p className="text-gray-500 text-sm">Address to your Place</p>
+                        {preInput('Address','Address to this place')}
                         <input type="text" placeholder="address"/>
-                        <h2 className="text-2xl mt-4">Photos</h2>
-                        <p className="text-gray-500 text-sm">more = better</p>
-                        <div className="flex">
+            
+                        {preInput('Photos','more = better')}
+                        <div className="flex gap-2">
                             <input type="text" placeholder={"Add using a link...........jpg"}/>
-                            <button className="bg-gray-200 grow">Add&nbsp; Photo</button>
+                            <button className="bg-gray-200 grow rounded-2xl">Add&nbsp; Photo</button>
                         </div>
                         <div className=" mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-                        <button className="border bg-transparent rounded-2xl p-8 text-xl text-gray-600">+</button>
+                        <button className=" flex justify-center  gap-1 border bg-transparent rounded-2xl p-8 text-xl text-gray-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+</svg>
 
+                           UPLOAD  
+                        </button>
+                        </div>
+                        {preInput('Description','description of the place')}
+                       
+                        <textarea/>
+                        {preInput('Perks','select all the perks of your place')}
+                        <div grid mt-2 gap-2 grid-cols-2 md:grid-cols-4 lg:grid-cols-6>
+                         <Perks selected={perks} onChange={setPerks}/>
+                         </div>
+                        
+                        {preInput('ExtraInfo','house rules, etc')}
+                       
+                        <textarea/>
+                        {preInput('Check in&out times','Add Check in and out times, remember to have some time window for cleaning the room between guests')}
+                       
+                        <div className="grid gap-2 sm:grid-cols-3">
 
+                        <div>
+                             <h3 className="mt-2 -mb-1"> Check in Time</h3>
+                            <input type="text" placeholder="14:00"/>
+                        </div>
+                        <div>
+                             <h3 className="mt-2 -mb-1"> Check out Time</h3>
+                            <input type="text"/>
+                        </div>
+                        <div>
+                             <h3 className="mt-2 -mb-1"> Max no. of guests</h3>
+                            <input type="text"/>
+                        </div>
+
+                        </div>
+                        <div>
+                            <button className="primary my-4">
+                                Save
+
+                            </button>
                         </div>
                     </form>
                 </div>
