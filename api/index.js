229d78bc9e494ecd8b28cac4d57b738757bc8727
Chetna1405 <122478476+Chetna1405@users.noreply.github.com>
2025-02-03 09:25:10 +0530

    page upload

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 const User = require('./models/user.js');
 const cookieParser = require('cookie-parser')
+const imageDownloader = require('image-downloader')
 require('dotenv').config()
 const app = express()
 
@@ -16,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin:'http://localhost:5173',
+    origin: 'http://localhost:5173',
 }))
 
 
@@ -57,15 +58,15 @@ app.post('/register', async (req, res) => {
 app.post('/login', async (req, res) => {
     const { email, password } = req.body;
     const userDoc = await User.findOne({
-        email        
+        email
     })
     if (userDoc) {
-        const passOk = bcrypt.compareSync(password , userDoc.password)
+        const passOk = bcrypt.compareSync(password, userDoc.password)
         if (passOk) {
             jwt.sign({
                 email: userDoc.email,
                 id: userDoc._id,
-            }, jwtSecret, {}, (err , token) => {
+            }, jwtSecret, {}, (err, token) => {
                 if (err) throw err;
                 res.cookie('token', token).json(userDoc);
             })
@@ -80,21 +81,33 @@ app.post('/login', async (req, res) => {
 app.get('/profile', (req, res) => {
     const { token } = req.cookies;
     if (token) {
-        jwt.verify(token, jwtSecret, {}, async (err , userData) => {
+        jwt.verify(token, jwtSecret, {}, async (err, userData) => {
             if (err) throw err;
-            const {name , email , _id} = await User.findById(userData.id);
-            
-            res.json({name ,email , _id});
+            const { name, email, _id } = await User.findById(userData.id);
+
+            res.json({ name, email, _id });
         })
     } else {
         res.json(null);
     }
-    
+
+})
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json(true);
 })
 
 
-app.post('/logout',(req,res)=>{
-    res.cookie('token','').json(true);
+//image 
+// console.log(__dirname); 
+app.post('/upload-by-link', async (req, res) => {
+    const { link } = req.body;
+    const newName = 'photo' + Date.now() + '.jpg';
+    await imageDownloader.image({
+        url: link,
+        dest: __dirname + '/uploads/' + newName ,
+    });
+    res.json(newName);
+
 })
 
 app.listen(4000, (req, res) => {
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,102 +1,130 @@
-import {Link, useParams} from "react-router-dom";
-import {useState} from "react";
+import { Link, useParams } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 import Perks from "../Perks";
 
 export default function PlacesPage() {
-    const {action} = useParams();
-    const [title,setTitle] = useState('');
-    const [address,setAddress] = useState('');
-    const [addedPhotos,setAddedPhotos] = useState([]);
-    const [photoLink,setPhotoLink] = useState('');
-    const [description,setDescription] = useState('');
-    const [perks,setPerks] = useState([]);
-    const [extraInfo,setExtraInfo] = useState('');
-    const [checkIn,setCheckIn] = useState('');
-    const [checkOut,setCheckOut] = useState('');
-    const [maxGuests,setMaxGuests] = useState(1);
-    function inputHeader(text){
-        return(
+    const { action } = useParams();
+    const [title, setTitle] = useState('');
+    const [address, setAddress] = useState('');
+    const [addedPhotos, setAddedPhotos] = useState([]);
+    const [photoLink, setPhotoLink] = useState('');
+    const [description, setDescription] = useState('');
+    const [perks, setPerks] = useState([]);
+    const [extraInfo, setExtraInfo] = useState('');
+    const [checkIn, setCheckIn] = useState('');
+    const [checkOut, setCheckOut] = useState('');
+    const [maxGuests, setMaxGuests] = useState(1);
+    function inputHeader(text) {
+        return (
             <h2 className="text-2xl mt-4">{text}</h2>
         );
     }
-    function inputDescription(text){
-        return(
+    function inputDescription(text) {
+        return (
             <p className="text-gray-500 text-sm">{text}</p>
 
         );
     }
-    function preInput(header,description){
-        return(
+    function preInput(header, description) {
+        return (
             <>
-            {inputHeader(header)}
-            {inputDescription(description)}
+                {inputHeader(header)}
+                {inputDescription(description)}
             </>
 
         );
     }
-    
+    async function addPhotoByLink(ev) {
+        ev.preventDefault();
+        const { data: filename } = await axios.post('/upload-by-link', { link: photoLink })
+        setAddedPhotos(prev => {
+            return [...prev + filename]
+        })
+        setPhotoLink('');
+    }
+
     return (
         <div>
             {action !== 'new' && (
-            <div className="text-center">
-                <Link className=" inline-flex gap-1 bg-primary text-white py-2 px-6 mt-6 rounded-full"  to={'/account/places/new'}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-</svg>
-                Add new Place
-             </Link>
-            </div>
-        )}
-            {action ==='new' && (
+                <div className="text-center">
+                    <Link className=" inline-flex gap-1 bg-primary text-white py-2 px-6 mt-6 rounded-full" to={'/account/places/new'}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                        Add new Place
+                    </Link>
+                </div>
+            )}
+            {action === 'new' && (
                 <div>
                     <form>
-                        {preInput('Title','Title for your Place, should be short and catchy as in advertisement')}
-                       
-                        <input type="text" placeholder="title, for example: My Lovely Apt"/>
-                        {preInput('Address','Address to this place')}
-                        <input type="text" placeholder="address"/>
-            
-                        {preInput('Photos','more = better')}
-                        <div className="flex gap-2">
-                            <input type="text" placeholder={"Add using a link...........jpg"}/>
-                            <button className="bg-gray-200 grow rounded-2xl">Add&nbsp; Photo</button>
-                        </div>
-                        <div className=" mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-                        <button className=" flex justify-center  gap-1 border bg-transparent rounded-2xl p-8 text-xl text-gray-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
-</svg>
+                        {preInput('Title', 'Title for your Place, should be short and catchy as in advertisement')}
 
-                           UPLOAD  
-                        </button>
+                        <input type="text" value={title} onChange={ev => setTitle(ev.target.value)} placeholder="title, for example: My Lovely Apt" />
+                        {preInput('Address', 'Address to this place')}
+                        <input type="text" value={address} onChange={ev => setAddress(ev.target.value)} placeholder="address" />
+
+                        {preInput('Photos', 'more = better')}
+                        <div className="flex gap-2">
+                            <input type="text"
+                                value={photoLink}
+                                onChange={ev => setPhotoLink(ev.target.value)}
+                                placeholder={"Add using a link...........jpg"} />
+                            <button onClick={addPhotoByLink} className="bg-gray-200 grow rounded-2xl">Add&nbsp; Photo</button>
                         </div>
-                        {preInput('Description','description of the place')}
-                       
-                        <textarea/>
-                        {preInput('Perks','select all the perks of your place')}
-                        <div grid mt-2 gap-2 grid-cols-2 md:grid-cols-4 lg:grid-cols-6>
-                         <Perks selected={perks} onChange={setPerks}/>
-                         </div>
-                        
-                        {preInput('ExtraInfo','house rules, etc')}
-                       
-                        <textarea/>
-                        {preInput('Check in&out times','Add Check in and out times, remember to have some time window for cleaning the room between guests')}
-                       
+                        <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+                            {addedPhotos.length > 0 && addedPhotos.map(link => (
+                                <div key={link}>
+                                    {link}
+                                </div>
+                            ))}
+                            <button className=" flex justify-center  gap-1 border bg-transparent rounded-2xl p-8 text-xl text-gray-600">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+                                </svg>
+
+                                UPLOAD
+                            </button>
+                        </div>
+                        {preInput('Description', 'description of the place')}
+
+                        <textarea value={description} onChange={ev => setDescription(ev.target.value)} />
+                        {preInput('Perks', 'select all the perks of your place')}
+                        <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
+                            <Perks selected={perks} onChange={setPerks} />
+                        </div>
+
+                        {preInput('ExtraInfo', 'house rules, etc')}
+
+                        <textarea value={extraInfo} onChange={ev => setExtraInfo(ev.target.value)} />
+                        {preInput('Check in&out times', 'Add Check in and out times, remember to have some time window for cleaning the room between guests')}
+
                         <div className="grid gap-2 sm:grid-cols-3">
 
-                        <div>
-                             <h3 className="mt-2 -mb-1"> Check in Time</h3>
-                            <input type="text" placeholder="14:00"/>
-                        </div>
-                        <div>
-                             <h3 className="mt-2 -mb-1"> Check out Time</h3>
-                            <input type="text"/>
-                        </div>
-                        <div>
-                             <h3 className="mt-2 -mb-1"> Max no. of guests</h3>
-                            <input type="text"/>
-                        </div>
+                            <div>
+                                <h3 className="mt-2 -mb-1"> Check in Time</h3>
+                                <input type="text"
+                                    placeholder="14:00"
+                                    value={checkIn}
+                                    onChange={ev => setCheckIn(ev.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <h3 className="mt-2 -mb-1"> Check out Time</h3>
+                                <input type="text"
+                                    placeholder="11"
+                                    value={checkOut}
+                                    onChange={ev => setCheckOut(ev.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <h3 className="mt-2 -mb-1"> Max no. of guests</h3>
+                                <input type="number"
+                                    value={maxGuests}
+                                    onChange={ev => setMaxGuests(ev.target.value)}
+                                />
+                            </div>
 
                         </div>
                         <div>
@@ -108,8 +136,8 @@ export default function PlacesPage() {
                     </form>
                 </div>
 
-            )} 
+            )}
         </div>
     );
-    
+
 }
