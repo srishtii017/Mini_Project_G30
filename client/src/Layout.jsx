3667b34e929219ffb0ed12c9ce14432b6628f25a
Anshita295 <anshitaagrawal295@gmail.com>
2025-02-08 14:12:15 +0530

    indexpage

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 export default function Layout() {
     return (
-        <div className="p-4">
+        <div className="p-4 px-8 flex flex-col min-h-screen">
             <Header />
             <Outlet />
         </div>
